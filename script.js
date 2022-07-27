437d2413aa0dd622246c307592109c7d386dd70d
unknown <sugamanandsahu01@gmail.com>
2022-07-27 15:31:48 +0530

    Changing codes at top and Adding Comments.

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-const select = (selector) => { // Select SINGLE ELEMENT
-    const select = document.querySelector(selector);
-    return select
-};
+const select = selector => document.querySelector(selector); // Select SINGLE ELEMENT
 
-const selectAll = (selector) => { // Select MULTIPLE ELEMENTS
-    const selectAll = document.querySelectorAll(selector);
-    return selectAll
-};
+const selectAll = selectorAll =>  document.querySelectorAll(selectorAll); // Select MULTIPLE ELEMENTS
 
-// Navbar open and close
+// Navbar OPEN and CLOSE on Icon CLICKS (MEDIUM and SMALL Devices)
 const hamburger = select(".hamburger-icon");
 hamburger.addEventListener("click", () => { // NAV OPEN Event Listener
     hamburger.classList.add("active");
     select("nav").classList.add("active");
+
+    // Add styles to SVGs
     select("#header .logo svg circle").classList.add("active");
     select("#header .logo svg g g path").classList.add("active");
     select("#header .logo svg g path").classList.add("active");
@@ -22,6 +18,8 @@ const nav_CLOSE = select(".nav-close-icon");
 nav_CLOSE.addEventListener("click", () => { // NAV CLOSE Event Listener
     hamburger.classList.remove("active");
     select("nav").classList.remove("active");
+
+    // Remove styles from SVGs
     select("#header .logo svg circle").classList.remove("active");
     select("#header .logo svg g g path").classList.remove("active");
     select("#header .logo svg g path").classList.remove("active");
