71490c824e54cc0b04a3066fbbf15dab2c1fb846
unknown <sugamanandsahu01@gmail.com>
2022-07-28 01:42:41 +0530

    Saving after clearing some spaces.

--- features.js
+++ features.js
@@ -2,22 +2,19 @@ const feature_CONTENT = [
     {
         image: "images/illustration-features-tab-1.svg",
         heading: "Bookmark in one click",
-        text: `Organize your bookmarks however you like. Our simple drag-and-drop interface 
-                gives you complete control over how you manage your favourite sites.`,
+        text: `Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.`,
         btnText: "More Info"
     },
     {
         image: "images/illustration-features-tab-2.svg",
         heading: "Intelligent search",
-        text: `Our powerful search feature will help you find saved sites in no time at all. 
-                No need to trawl through all of your bookmarks.`,
+        text: `Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.`,
         btnText: "More Info"
     },
     {
         image: "images/illustration-features-tab-3.svg",
         heading: "Share your bookmarks",
-        text: `Easily share your bookmarks and collections with others. Create a shareable 
-                link that you can send at the click of a button.`,
+        text: `Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.`,
         btnText: "More Info"
     }
 ]
