dc3b2445622211c19b99e3d4a3383e44679b93a3
unknown <sugamanandsahu01@gmail.com>
2022-07-27 23:50:24 +0530

    Adding all the features' contents to features.js

---
+++ features.js
@@ -0,0 +1,25 @@
+const feature_CONTENT = [
+    {
+        image: "images/illustration-features-tab-1.svg",
+        heading: "Bookmark in one click",
+        text: `Organize your bookmarks however you like. Our simple drag-and-drop interface 
+                gives you complete control over how you manage your favourite sites.`,
+        btnText: "More Info"
+    },
+    {
+        image: "images/illustration-features-tab-2.svg",
+        heading: "Intelligent search",
+        text: `Our powerful search feature will help you find saved sites in no time at all. 
+                No need to trawl through all of your bookmarks.`,
+        btnText: "More Info"
+    },
+    {
+        image: "images/illustration-features-tab-3.svg",
+        heading: "Share your bookmarks",
+        text: `Easily share your bookmarks and collections with others. Create a shareable 
+                link that you can send at the click of a button.`,
+        btnText: "More Info"
+    }
+]
+
+export default feature_CONTENT;
